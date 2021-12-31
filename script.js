a5cf2163f8dbee3bb18e2d8d5b020922381ea6fc
abucchianico <92989566+abucchianico@users.noreply.github.com>
2021-12-31 14:12:14 -0300

    Minimum functional state

--- script.js
+++ script.js
@@ -3,61 +3,82 @@
  * Copyright © 2021 Anderson Bucchianico. All rights reserved.
 */
 
-let stickyNote = document.querySelector("div[class='postit']");
-stickyNote.addEventListener('dragstart',dragStart);
-stickyNote.addEventListener('dragend',dragEnd);
+let stickyNoteStack = document.querySelector("#stickynote-stack");
+stickyNoteStack.addEventListener('dragstart',StackdragStart);
+stickyNoteStack.addEventListener('dragend',StackdragEnd);
 
-function createColumn() {
-    let column = document.importNode(document.querySelector("template[id='column']").content,true);
-    createColumnItem(column);
-    document.querySelector("article[class='board']").appendChild(column);
+const boardColumns = document.querySelector('ul[class="board"]');
+
+function StackdragStart(event) {
+    console.log('dragStart',event);
+
+    function insertStickynoteSlotsOnColumns() {
+        for (let column of boardColumns.children) {
+            let stickynoteSlot = document.importNode(document.querySelector("template[id='stickynote-slot']").content,true);
+            stickynoteSlot.firstElementChild.addEventListener('dragover', stickynoteSlotDragover);
+            stickynoteSlot.firstElementChild.addEventListener('drop', stickynoteSlotDrop);
+            column.lastElementChild.appendChild(stickynoteSlot);
+        }
+    }
+    function insertNewColumnSlotOnBoard() {
+        let newColumn = document.importNode(document.querySelector("template[id='column']").content,true);
+        newColumn.firstElementChild.addEventListener('dragover',columnDragover);
+        newColumn.firstElementChild.addEventListener('drop',columnDrop);
+        boardColumns.appendChild(newColumn);
+    }
+
+    insertStickynoteSlotsOnColumns();
+    insertNewColumnSlotOnBoard();
 }
 
-function createColumnItem(column) {
-    let item = document.importNode(document.querySelector("template[id='item-slot']").content,true);
-    addEventListenersfor(item.firstElementChild);
-    column.querySelector(".column-subcontainer").appendChild(item);
+function StackdragEnd(event) {
+    console.log('dragEnd',event);
+
+    function removeExistingColumnSlots() {
+        for (let columnslot of boardColumns.children) {
+            if (columnslot.className == 'column-list-slot') {
+                columnslot.parentElement.removeChild(columnslot);
+            }
+        }
+    }
+    function removeExistingStickynoteSlots() {
+        for (let columns of boardColumns.children) {
+            for (let itemslot of columns.lastElementChild.children) {
+                if (itemslot.className == 'stickynote-list-slot') {
+                    itemslot.parentElement.removeChild(itemslot);
+                }
+            }
+        }
+    }
+    removeExistingColumnSlots();
+    removeExistingStickynoteSlots();
 }
 
-function addEventListenersfor(item) {
-    item.addEventListener('dragover',dragOver);
-    item.addEventListener('dragEnter',dragEnter);
-    item.addEventListener('dragleave',dragLeave);
-    item.addEventListener('drop',dragDrop);
-}
-
-function dragStart(event) {
-    console.log("sticky dragStart");
-    createColumn();
-    Array.from(document.querySelectorAll("ul[class='list-item']")).forEach( element => {
-        element.classList.add('list-slot');
-    });
-}
-
-function dragEnd(event) {
-    console.log("sticky dragEnd",Array.from(document.querySelectorAll("ul.list-slot")));
-    Array.from(document.querySelectorAll("ul.list-slot")).forEach( element => {
-        element.classList.remove('list-slot');
-    });
-}
-
-function dragOver(event) {
-    console.log('item dragOver');
+function columnDragover(event) {
     event.preventDefault();
 }
+function columnDrop(event) {
+    console.log('drop',event);
 
-function dragEnter(event) {
+    function convertColumnSlotToColumn(event) {
+        event.target.parentElement.className = 'column-list';
+        event.target.parentElement.removeEventListener('drop',columnDrop);
+        event.target.parentElement.removeEventListener('dragover',columnDragover);
+        let newItem = document.importNode(document.querySelector("template[id='stickynote']").content,true);
+        event.target.appendChild(newItem);
+    }
+    convertColumnSlotToColumn(event);
+}
+
+function stickynoteSlotDrop(event) {
+    console.log('stickynoteDrop',event);
+    function insertNewStickynoteItemOnSlot(event) {
+        let newItem = document.importNode(document.querySelector("template[id='stickynote']").content,true);
+        event.target.parentElement.appendChild(newItem);
+    }
+    insertNewStickynoteItemOnSlot(event);
+}
+
+function stickynoteSlotDragover(event) {
     event.preventDefault();
-    console.log('item dragEnter');
-}
-
-function dragLeave(event) {
-    console.log('item dragLeave');
-}
-
-function dragDrop(event) {
-    console.log('item dragDrop');
-    let newItem = document.importNode(document.querySelector("template[id='item-sticky']").content,true);
-    event.target.appendChild(newItem);
-    console.log(event);
 }
